916a4dffe39652bc75120f55ff31a9ef3b1bcddb
JanDirkKoersma <75176681+JanDirkKoersma@users.noreply.github.com>
2021-03-07 12:01:15 +0100

    Add files via upload

---
+++ Javascript_003/script.js
@@ -0,0 +1,26 @@
+const age = 18;
+
+if (age >= 18) {
+  console.log("Enter");
+} else {
+  console.log("Do not Enter");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+  console.log("Enter")
+} else {
+  console.log("Do not Enter");
+}
+
+const driverStatus = 'bob'
+
+if (driverStatus == 'bob') {
+  console.log("Drive")
+} else {
+  console.log("Do not Drive")
+}
+
+
+
